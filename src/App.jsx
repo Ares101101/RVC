aabaa74df7b221352684dc3f9e3af06492281670
Ares <diego.amachiflores@gmail.com>
2023-10-31 16:56:52 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { MenuHeader } from "./components/menuHeader"
+
+
+function App() {
+  
+  return (
+    <div>
+     <MenuHeader/>
+     
+    </div>
+     
+
+  )
+}
+
+export default App
