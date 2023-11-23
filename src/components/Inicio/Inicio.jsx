90f955eeabbfdd8baf3c9763e9c6b9350fee19b3
Ares <diego.amachiflores@gmail.com>
2023-11-23 17:07:20 -0500

    23/11/2023

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -15,7 +15,7 @@ export default function Inicio (){
     
     return(
         <article className="flex justify-center w-full pt-[192px]  ">
-            <div className=" flex flex-col justify-stard h-full w-[1280px]  bg-white ">
+            <div className=" flex flex-col justify-stard h-full w-[100%] max-w-[1280px] bg-white ">
                 <div className=" w-[100%] relative flex  items-center flex-col justify-center max-h-96 overflow-hidden" >
                     <Slider indexFunc={indexFunc}/>    
                     <SliderText  index={indexSlider}/>
--- src/components/Inicio/serviciosSlider/servSlider.jsx
+++ src/components/Inicio/serviciosSlider/servSlider.jsx
@@ -2,27 +2,53 @@ import { Link } from "react-router-dom";
 import { juan } from "../RVC/n";
 import { Scard } from "../../servicios/serviciosCard.jsx/scard"
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Pagination, Navigation, EffectCoverflow} from 'swiper/modules'
+import { Navigation, Scrollbar,  } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export function ServSlider (){      
 
     return(
-        <div className="flex w-full  items-center justify-initial  ">
+        <div className="flex w-full h-[400px] items-center justify-center p-4">
             <Swiper 
-                centeredSlides={true}
-                grabCursor={true}
-                pagination={{ clickable: true }}
-                autoplay={{ delay: 3000 }}
-                className="flex"
-                modules={[EffectCoverflow, Navigation,Pagination]}
-                
+            modules={[Navigation, Scrollbar, ]}
+            slidesPerView={1}
+            spaceBetween={14}
+            scrollbar={{ draggable: true }}
+            pagination={{
+                clickable: true,
+              }}
+            breakpoints={{
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+                1024: {
+                  slidesPerView: 4,
+                  spaceBetween: 50,
+                },
+                1280:{
+                  slidesPerView: 5,
+                  spaceBetween: 50,
+                }
+            }}
+            className="h-full flex w-[100%] items-center justify-center"
             >
-                <div className="flex gap-3 ">
                 {juan.map((element) => (
-                <SwiperSlide key={element.id}>
+                <SwiperSlide 
+                key={element.id} 
+                virtualIndex={element.id}
+                
+                >
                     <Link 
                         to={`/SERVICIOS/${element.id}`} 
-                        >
+                        >   
                         <Scard 
                             cover={element.imagen} 
                             title={element.title} 
@@ -30,10 +56,9 @@ export function ServSlider (){
                     </Link>       
                 </SwiperSlide>  
                 ))}       
-                </div>
-            </Swiper>  
+            </Swiper>                    
         </div>
-        
+           
     )
 }
 
