0829a382962677a82697b7016aa62c1b5a75e263
Ares <diego.amachiflores@gmail.com>
2024-01-08 12:48:39 -0500

    08

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://ares101101.github.io/RVC/'
+  base:''
 })
